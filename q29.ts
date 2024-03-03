d6ffc0193cde72e884639ebc9ed60d67bb4eff78
munazzahrao <161147860+munazzahrao@users.noreply.github.com>
2024-03-03 08:16:49 +0500

    First commit

---
+++ q29.ts
@@ -0,0 +1,13 @@
+// Question no 29
+
+let favourite_fruits: string[] = ['banana', 'apple', 'mango'];
+
+if (favourite_fruits.indexOf('banana')!== -1) {
+    console.log('you really like banans');
+}
+if (favourite_fruits.indexOf('apple')!== -1) {
+    console.log('you really like apple');
+}
+if (favourite_fruits.indexOf('mango')!== -1) {
+    console.log('you really like mango');
+}
